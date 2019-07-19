0b0efe94759b398b0724d6144dc334813f50f32f
Gábor Trajtler <trajtlerg@gmail.com>
2019-07-19 14:31:37 +0200

    Implement "Add new task" story

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 #include <fstream>
 
 void printMenu();
-void listTasks(std::string taskFilePath);
 
-int main(int argc, char* argv[])
+int listTasks(std::string taskFilePath);
+
+bool fileIsEmpty(std::ifstream &pFile);
+
+int tasksFileNotFound();
+
+int addTask(const std::string& taskFilePath, const std::string& taskDescription);
+
+int main(int argc, char *argv[])
 {
     if (argc == 1) {
         printMenu();
@@ -14,10 +21,10 @@ int main(int argc, char* argv[])
     std::string taskFilePath = "../tasks";
     std::string commandArg = std::string(argv[1]);
 
-    if(commandArg == "-l"){
-        listTasks(taskFilePath);
+    if (commandArg == "-l") {
+        return listTasks(taskFilePath);
     } else if (commandArg == "-a") {
-        //TODO
+        return addTask(taskFilePath, std::string(argv[2]));
     } else if (commandArg == "-r") {
         //TODO
     } else if (commandArg == "-c") {
@@ -27,7 +34,10 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-void printMenu() {
+
+
+void printMenu()
+{
     std::cout << std::endl;
     std::cout << "Command Line Todo application" << std::endl;
     std::cout << "=============================" << std::endl;
@@ -40,18 +50,50 @@ void printMenu() {
     std::cout << std::endl;
 }
 
-void listTasks(std::string taskFilePath) {
+int listTasks(std::string taskFilePath)
+{
     std::ifstream taskFile(taskFilePath);
     std::string line;
     int lineCount = 1;
     std::cout << std::endl;
     if (taskFile.is_open()) {
-        while (getline(taskFile, line)) {
-            std::cout << lineCount << " - " << line << '\n';
-            lineCount++;
+        if (fileIsEmpty(taskFile)) {
+            std::cout << "No todos for today! :)" << std::endl;
+        } else {
+            while (getline(taskFile, line)) {
+                std::cout << lineCount << " - " << line << '\n';
+                lineCount++;
+            }
         }
         taskFile.close();
     } else {
-        std::cout << "File does not exist!";
+        return tasksFileNotFound();
     }
+    return 0;
+}
+
+int addTask(const std::string& taskFilePath, const std::string& taskDescription)
+{
+    std::ofstream taskFile(taskFilePath, std::ios::app);
+    std::cout << std::endl;
+    if (taskFile.is_open()) {
+        taskFile << taskDescription << std::endl;
+        taskFile.close();
+    } else {
+        return tasksFileNotFound();
+    }
+    std::cout << "New task added: " << taskDescription << std::endl;
+    std::cout << std::endl;
+    return 0;
+}
+
+bool fileIsEmpty(std::ifstream &pFile)
+{
+    return pFile.peek() == std::ifstream::traits_type::eof();
+}
+
+int tasksFileNotFound()
+{
+    std::cout << "Tasks file does not exist! Exiting program." << std::endl;
+    return 1;
 }
